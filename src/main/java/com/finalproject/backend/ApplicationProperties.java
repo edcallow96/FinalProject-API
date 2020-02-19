d771a7d6fff0b7df61a7f19b9a54ce0c045ab5cc
ecallow <ed.callow@capgemini.com>
2020-02-19 21:51:19 +0000

    Clean up

--- src/main/java/com/finalproject/backend/ApplicationProperties.java
+++ src/main/java/com/finalproject/backend/ApplicationProperties.java
@@ -3,14 +3,12 @@ package com.finalproject.backend;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
 
 @ConfigurationProperties
 @Getter
 @Setter
-@Component
 public class ApplicationProperties {
   private String deepSecureApiKey;
   private Path downloadDirectory;
