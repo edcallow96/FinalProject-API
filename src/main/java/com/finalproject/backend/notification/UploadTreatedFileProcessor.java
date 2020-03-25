9462d1709cd5c69748bf9022ee702890b004c433
ecallow <ed.callow@capgemini.com>
2020-03-25 19:07:21 +0000

    Upload processed files with SSE information.

--- src/main/java/com/finalproject/backend/notification/UploadTreatedFileProcessor.java
+++ src/main/java/com/finalproject/backend/notification/UploadTreatedFileProcessor.java
@@ -1,12 +1,17 @@
 package com.finalproject.backend.notification;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.finalproject.backend.ApplicationProperties;
 import com.finalproject.backend.model.ProcessJob;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
+
+import static com.amazonaws.services.s3.model.ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION;
+import static org.apache.commons.io.FileUtils.openInputStream;
 
 @Component
 @Slf4j
@@ -21,11 +26,14 @@ public class UploadTreatedFileProcessor implements Processor {
   }
 
   @Override
+  @SneakyThrows
   public void process(Exchange exchange) {
     ProcessJob processJob = exchange.getIn().getBody(ProcessJob.class);
     String locationWithinBucket = applicationProperties.getDownloadDirectory().relativize(processJob.getPayloadLocation().toPath()).toString();
     log.info("Location within bucket {}", locationWithinBucket);
-    amazonS3.putObject(applicationProperties.getTreatedBucketName(), locationWithinBucket, processJob.getPayloadLocation());
+    ObjectMetadata metadata = new ObjectMetadata();
+    metadata.setSSEAlgorithm(AES_256_SERVER_SIDE_ENCRYPTION);
+    amazonS3.putObject(applicationProperties.getTreatedBucketName(), locationWithinBucket, openInputStream(processJob.getPayloadLocation()), metadata);
     processJob.setTreatedBucketKey(locationWithinBucket);
   }
 }
--- src/test/java/com/finalproject/backend/notification/UploadTreatedFileProcessorShould.java
+++ src/test/java/com/finalproject/backend/notification/UploadTreatedFileProcessorShould.java
@@ -1,6 +1,7 @@
 package com.finalproject.backend.notification;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.finalproject.backend.ApplicationProperties;
 import com.finalproject.backend.model.ProcessJob;
 import org.apache.camel.Exchange;
@@ -10,6 +11,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
@@ -17,11 +20,14 @@ import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import static com.amazonaws.services.s3.model.ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -42,15 +48,20 @@ public class UploadTreatedFileProcessorShould {
   @InjectMocks
   private UploadTreatedFileProcessor uploadTreatedFileProcessor;
 
+  @Captor
+  private ArgumentCaptor<ObjectMetadata> objectMetadataArgumentCaptor;
+
   private Exchange exchange;
 
   @Before
   public void setUp() throws IOException {
     when(applicationProperties.getTreatedBucketName()).thenReturn(randomAlphabetic(10));
     when(applicationProperties.getDownloadDirectory()).thenReturn(temporaryFolder.newFolder(randomAlphabetic(10)).toPath());
+    File treatedFile = applicationProperties.getDownloadDirectory().resolve(randomAlphabetic(10)).toFile();
+    treatedFile.createNewFile();
     exchange = ExchangeBuilder.anExchange(new DefaultCamelContext()).withBody(
         ProcessJob.builder()
-            .payloadLocation(applicationProperties.getDownloadDirectory().resolve(randomAlphabetic(10)).toFile())
+            .payloadLocation(treatedFile)
             .build()
     ).build();
   }
@@ -61,7 +72,16 @@ public class UploadTreatedFileProcessorShould {
 
     File processedFile = exchange.getIn().getBody(ProcessJob.class).getPayloadLocation();
 
-    verify(amazonS3).putObject(applicationProperties.getTreatedBucketName(), processedFile.getName(), processedFile);
+    verify(amazonS3).putObject(eq(applicationProperties.getTreatedBucketName()), eq(processedFile.getName()), any(FileInputStream.class), any(ObjectMetadata.class));
+  }
+
+  @Test
+  public void uploadsProcessedFileWithSSE() {
+    uploadTreatedFileProcessor.process(exchange);
+
+    verify(amazonS3).putObject(eq(applicationProperties.getTreatedBucketName()), anyString(), any(FileInputStream.class), objectMetadataArgumentCaptor.capture());
+
+    assertThat(objectMetadataArgumentCaptor.getValue().getSSEAlgorithm(), equalTo(AES_256_SERVER_SIDE_ENCRYPTION));
   }
 
   @Test
